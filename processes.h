e6f9c42203cf463e666d885a93b2dc13e926954c
RawanFekry <rawanmohamedfekry@gmail.com>
2022-01-02 19:43:25 +0200

    Search and Sort

--- processes.h
+++ processes.h
@@ -8,23 +8,23 @@ class Processes
 {
 public:
     Processes();
-    QVector<Shape>shapes;
-    Shape search(QString shapeName );
-    void add(Shape& shape);
+    QVector<Shape*>*shapes;
+    Shape* search(QString shapeName );
+    void add(Shape* shape);
     struct  Check_Asc
     {
-         bool operator()( const Shape & s1, const Shape& s2 ) const {
-            return s1.get_perimeter() < s2.get_perimeter();
+         bool operator()( const Shape* s1, const Shape* s2 ) const {
+            return s1->get_perimeter() < s2->get_perimeter();
         }
     };
     struct  Check_Des
     {
-         bool operator()( const Shape & s1, const Shape& s2 ) const {
-            return s1.get_perimeter() > s2.get_perimeter();
+         bool operator()( const Shape* s1, const Shape* s2 ) const {
+            return s1->get_perimeter() > s2->get_perimeter();
         }
     };
-    QVector<Shape> sort_Ascending (QVector <Shape> shapes);
-     QVector<Shape> sort_Descending (QVector <Shape> shapes);
+    QVector<Shape*> sort_Ascending (QVector <Shape*> *shapes);
+     QVector<Shape*> sort_Descending (QVector <Shape*> *shapes);
 
 
 };
